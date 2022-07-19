a3c9d4768607557cd0a4df694f3e000f14e4b0c9
boldyrev1703 <litovchenko.73@list.ru>
2022-07-19 22:13:59 +0300

    state for chekbox

--- shop/components/filters/chekList/checkList.tsx
+++ shop/components/filters/chekList/checkList.tsx
@@ -1,17 +1,27 @@
 import css from './checkList.module.css';
 import { v4 as uuidv4 } from 'uuid';
+import { useMergeState } from '../../../utilsFunctions/useHook';
+import { ChangeEvent, ChangeEventHandler, useEffect } from 'react';
 
 type CheckBoxProps = {
     label: string,
     value: string,
     name: string,
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void,
 }
 
-const CheckBox = ({label, value, name}: CheckBoxProps) => {
+const CheckBox = ({label, value, name, onChange}: CheckBoxProps) => {
     const id = uuidv4();
     return (
         <>
-            <input type="checkbox" id={id} className={css['custom-checkbox']} name={name}value={value} />
+            <input 
+                type="checkbox"
+                id={id} 
+                className={css['custom-checkbox']} 
+                name={name} 
+                value={value} 
+                onChange={e => onChange(e)}
+            />
             <label htmlFor={id}>{label}</label>
         </>
     )
@@ -37,12 +47,27 @@ const MOKED_CHEKBOCKS = [
 ];
 
 export const CheckBoxFilter = () => {
-    const name = uuidv4()
+    const name = uuidv4();
+    const initState = MOKED_CHEKBOCKS.reduce((o, key) => Object.assign(o, {[key.value]: false}), {});
+    
+    const [chekedParams, setChekedParams] = useMergeState({...initState});
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        //@ts-ignore
+        setChekedParams(Object.assign(chekedParams, {[e.target.value]: !chekedParams[key]}));
+    }
+
     return (
         <div className={css.wrap}>
+            {/* label нужно прокинуть пропсой */}
             <label className={css.label}>asd</label>
             {MOKED_CHEKBOCKS.map(el => 
-                <CheckBox label={el.label} value={el.value} name={name} />  
+                <CheckBox 
+                    label={el.label} 
+                    value={el.value} 
+                    name={name} 
+                    onChange={onChange}
+                />  
             )}
         </div>
     );

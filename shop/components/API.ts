40d3b79b6af772ec590b5bd947aa9031e7ddcc44
Evgeniy Finskiy <finskiy.ru@yandex.ru>
2023-02-19 18:18:36 +0300

    Init

--- shop/components/API.ts
+++ shop/components/API.ts
@@ -1,11 +1,11 @@
 const api = 'http://dockerbackend';
-const GET_CATEGORY = `${api}/category`;
+const GET_CATEGORY = `${api}/product/category`;
 const GET_PRODUCTS_BY_ID = `${api}/product/get_by_category?id=`;
 const GET_MAIN_SWIPER = `${api}/ic/get_active`;
 const GET_LATEST_PRODUCT = `${api}/product/get_latest`;
 const GET_PRODUCT_BY_SUBCATEGORY_ID = `${api}/product/get_by_subcategory?id=`
-const GET_CATEGORY_FILTER = `${api}/filters/get_by_category?id=`
-const AUTH_USER = `${api}/auth/login`;
+const GET_CATEGORY_FILTER = `${api}/product/filters/get_by_category?id=`
+const AUTH_USER = `${api}/user/auth/login`;
 const GET_PRODUCT_BY_ID = `${api}/product/`;
 
 export const API = {

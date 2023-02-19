421df19a796e6fd061819a8e5b1739d77832e074
Evgeniy Finskiy <finskiy.ru@yandex.ru>
2023-02-19 18:35:21 +0300

    Ignore Eslint while build

--- shop/next.config.js
+++ shop/next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
   eslint: {
-    ignoreDuringBuilds: true
-  }
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 
